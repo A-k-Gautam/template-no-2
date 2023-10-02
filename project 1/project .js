a2c685246b76b5ea615a2c2332c01bb909623015
Gautam <138514771+A-k-Gautam@users.noreply.github.com>
2023-10-02 20:19:20 +0530

    Add files via upload

---
+++ project 1/project .js
@@ -0,0 +1,121 @@
+var openBtn = document.getElementById('openBtn');
+var closeBtn = document.getElementById('closeBtn');
+var sidebar = document.getElementById('sidebar');
+
+openBtn.addEventListener('click', function() {
+  sidebar.style.left = '0';
+  openBtn.style.display = 'none';
+  closeBtn.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', function() {
+  sidebar.style.left = '-250px';
+  openBtn.style.display = 'block';
+  closeBtn.style.display = 'none';
+});
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+<section id="hero">
+    <div class="container">
+      <div>
+        <img src="banner.jpg" class="img-fluid" alt="copule img">
+      </div>
+      <div class="row">
+        <div class="col-sm-12 col-md-3 col-lg-3 col-12">
+          <div class="card" style="width: 16rem; height: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title mt-2">01</h5>
+              <h6 class="card-subtitle mb-2 mt-4">What we do</h6>
+              <p class="card-text mt-4">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-3 col-lg-3 col-12">
+          <div class="row">
+            <div class="card" style="width: 16rem; height: 18rem;">
+              <div class="card-body">
+                <h5 class="card-title mt-2">02</h5>
+                <h6 class="card-subtitle mb-2 mt-4">Professionals</h6>
+                <p class="card-text mt-4">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-3 col-lg-3 col-12">
+            <div class="card" style="width: 16rem; height: 18rem;">
+              <div class="card-body">
+                <h5 class="card-title mt-2">03</h5>
+                <h6 class="card-subtitle mb-2 mt-4 ">Delicious Breads</h6>
+                <p class="card-text mt-4">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-3 col-lg-3 col-12">
+            <div class="card" style="width: 16rem; height: 18rem;">
+              <div class="card-body">
+                <h5 class="card-title mt-2">04</h5>
+                <h6 class="card-subtitle mb-2 mt-4 ">Our History</h6>
+                <p class="card-text mt-4">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  </section>
+  <section>
+    <div class="col-sm-12 col-md-12 col-lg-12 col-12">
+      <div class="head">Our Traditions</div>
+      <!-- <div class="row">
+          <div class="col-sm-6 mb-3 mb-sm-0">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Freshest baked treats</h5>
+                <p class="card-text">Duis aute irure dolor in reprehenderit in voluptate velit </br>esse cillum dolore eu fugiat nulla pariatur. Excepteur </br>sint occaecat cupidatat non proident.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Bread Difference</h5>
+                <p class="card-text">Duis aute irure dolor in reprehenderit in voluptate velit </br>esse cillum dolore eu fugiat nulla pariatur. Excepteur </br>sint occaecat cupidatat non proident.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-6 mb-3 mb-sm-0">
+            <div class="card">
+              <div class="card-body">
+                <h6 class="card-title">Carefully sourced ingredients</h6  >
+                <p class="card-text">Duis aute irure dolor in reprehenderit in voluptate velit </br>esse cillum dolore eu fugiat nulla pariatur. Excepteur </br>sint occaecat cupidatat non proident.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title"> Handmade with select flours</h5>
+                <p class="card-text">Duis aute irure dolor in reprehenderit in voluptate velit </br>esse cillum dolore eu fugiat nulla pariatur. Excepteur </br>sint occaecat cupidatat non proident.</p>
+              </div>
+            </div>
+          </div>
+        </div> -->
+    </div>
+    </div>
+  </section>
